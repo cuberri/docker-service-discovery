1cd5ebb1930282d270f9133567790e5d51644e9a
cuberri <cuberri@gmail.com>
2014-02-10 23:52:28 +0100

    updated appserver output

--- serf-solution/appserver/appserver.py
+++ serf-solution/appserver/appserver.py
@@ -3,6 +3,6 @@ from bottle import route, run, template, request
 
 @route('/hello/:name')
 def index(name='World'):
-    return template('Hello {{name}} ({{remote_addr}}), I\'m {{hostname}} !\n', name=name, remote_addr=request.remote_addr, hostname=platform.node())
+    return template('Hello {{name}}, I\'m {{hostname}} !\n', name=name, hostname=platform.node())
 
 run(host='0.0.0.0', port=8080)
